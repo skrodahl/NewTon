bc80a92510bce35da50e0c28f897982e8bf6cb01
Havard Skrodahl <skrodahl@gmail.com>
2025-09-22 15:55:15 +0200

    Changed version number to 2.0.2-beta

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let matches = [];
 let currentStatsPlayer = null;
 
 // Application version
-const APP_VERSION = '2.0.2';
+const APP_VERSION = '2.0.2-beta';
 
 // =============================================================================
 // DIALOG STACK MANAGER - Unified dialog stacking system
