86a37b20a9096d660c94d03edcdae2b53ea70025
Havard Skrodahl <skrodahl@gmail.com>
2025-10-13 15:02:10 +0200

    Bumped version number to v.2.5.0 in main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let matches = [];
 let currentStatsPlayer = null;
 
 // Application version
-const APP_VERSION = '2.5.0-beta';
+const APP_VERSION = '2.5.0';
 
 // Application identity (encoded)
 const _0x4e = [78,101,119,84,111,110,32,68,67,32,84,111,117,114];
