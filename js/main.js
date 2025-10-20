86321666bfd2a04f32976e18825aff6941335ba0
Havard Skrodahl <skrodahl@gmail.com>
2025-10-20 15:41:22 +0200

    Updated version number

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let matches = [];
 let currentStatsPlayer = null;
 
 // Application version
-const APP_VERSION = '3.0.0';
+const APP_VERSION = '3.0.1';
 
 // Application identity (encoded)
 const _0x4e = [78,101,119,84,111,110,32,68,67,32,84,111,117,114];
