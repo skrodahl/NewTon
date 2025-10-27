3370c51f5245bd42df2c29535eb9c326f4b3db8f
Havard Skrodahl <skrodahl@gmail.com>
2025-10-27 14:16:31 +0100

    Bumped version to v4.0.2-beta in js/main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let matches = [];
 let currentStatsPlayer = null;
 
 // Application version
-const APP_VERSION = '4.0.1';
+const APP_VERSION = '4.0.2-beta';
 
 // Application identity (encoded)
 const _0x4e = [78,101,119,84,111,110,32,68,67,32,84,111,117,114];
