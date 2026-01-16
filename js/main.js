3afa051645f7e70e1e02d32bd42905cac51cc41a
Havard Skrodahl <skrodahl@gmail.com>
2026-01-17 00:06:51 +0100

    Bumped version to v4.1.20

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let matches = [];
 let currentStatsPlayer = null;
 
 // Application version
-const APP_VERSION = '4.1.19';
+const APP_VERSION = '4.1.20';
 
 // Application identity (encoded)
 const _0x4e = [78,101,119,84,111,110,32,68,67,32,84,111,117,114];
